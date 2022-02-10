d33631378ba0c348168c99660be71700848259c9
WeitaLi <liweita11@gmail.com>
2022-02-10 11:55:32 -0500

    changed dir

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,14 @@ public class Main{
 		game.setPlayer2(new AIPlayer('B', board, "Computer"));
 
 		game.playGame();
+//		int[][] test = {{1,2,3},{4,5,6}, {7,8,9}};
+//		int[][] testCopy = new int[3][3];
+//		System.out.println(testCopy[0][0] == '\u0000');
 	}
+//	char [] test = new char [4];
+//		test[0] = 'a';
+//		test[1] = 'a';
+//		test[2] = 'n';
+//		test[3] = 'a';
+//		System.out.println(board.duplicates(test, 'a'));
 }
